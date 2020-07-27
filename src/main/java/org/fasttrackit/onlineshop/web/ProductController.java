552edbf1935896fa14613189c64c51fc7c64bc36
maraalexandra <mara.alexandra11@yahoo.com>
2020-07-27 17:41:49 +0300

    implemented CRUD endpoints for products

--- src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -2,8 +2,11 @@ package org.fasttrackit.onlineshop.web;
 
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.GetProductsRequest;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,4 +38,27 @@ public class ProductController {
 
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
+        final Product product = productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getProducts(@Valid GetProductsRequest request, Pageable pageable) {
+        final Page<Product> products = productService.getProducts(request, pageable);
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+
+//    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
+    productService.deleteProduct(id);
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
